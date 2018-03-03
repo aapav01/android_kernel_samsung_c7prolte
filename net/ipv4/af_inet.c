924e9b2bfcf6b0a88d613396c8efc400b38b40e2
Apavayan <info@apavayan.com>
2018-03-03 07:37:59 +0530

    net: ipv4: On Disable Samsung knox ncm compile error
    Signed-off-by: Apavayan <info@apavayan.com>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -412,6 +412,7 @@ out_rcu_unlock:
 }
 
 /* START_OF_KNOX_NPA */
+#ifdef CONFIG_KNOX_NCM
 /** The function is used to check if the ncm feature is enabled or not; if enabled then collect the socket meta-data information; **/
 static void knox_collect_metadata(struct socket *sock) {
     if(check_ncm_flag()) {
@@ -560,6 +561,7 @@ static void knox_collect_metadata(struct socket *sock) {
         insert_data_kfifo_kthread(ksm);
     }
 }
+#endif
 /* END_OF_KNOX_NPA */
 
 /*
@@ -591,7 +593,9 @@ int inet_release(struct socket *sock)
 		    !(current->flags & PF_EXITING))
 			timeout = sk->sk_lingertime;
         /* START_OF_KNOX_NPA */
+#ifdef CONFIG_KNOX_NCM
         knox_collect_metadata(sock);
+#endif
         /* END_OF_KNOX_NPA */
 		sock->sk = NULL;
 		sk->sk_prot->close(sk, timeout);
